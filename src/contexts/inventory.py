153a3767215646ec2e5226d5a0060064e8dafbde
Brandon Semilla <semibran@gmail.com>
2021-04-12 14:35:03 -0700

    inventory grid entrance animation

--- src/contexts/inventory.py
+++ src/contexts/inventory.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 from pygame import Surface
 from assets import load as use_assets
@@ -7,8 +8,13 @@ from statusbar import StatusBar
 from keyboard import key_times
 import palette
 
+from anims.tween import TweenAnim
+from easing.expo import ease_out
+
 SPACING = 4
 MARGIN = 8
+ENTER_DURATION = 8
+ENTER_STAGGER = 4
 
 class InventoryContext(Context):
   def __init__(ctx, parent, inventory, on_close=None):
@@ -17,12 +23,32 @@ class InventoryContext(Context):
     ctx.grid_size =  (inventory.cols, inventory.rows)
     ctx.on_close = on_close
     ctx.cursor = (0, 0)
+    ctx.active = True
+    ctx.anims = []
     ctx.bar = StatusBar()
     item = ctx.get_item_at(ctx.cursor)
     if item:
       ctx.select_item(item)
     else:
       ctx.bar.print("Your pack is currently empty.")
+    ctx.enter()
+
+  def enter(ctx):
+    cols, rows = ctx.grid_size
+    ctx.active = True
+    cells = []
+    for row in range(rows):
+      for col in range(cols):
+        cells.append((col, row))
+    random.shuffle(cells)
+    index = 0
+    for col, row in cells:
+      ctx.anims.append(TweenAnim(
+          duration=ENTER_DURATION,
+          delay=ENTER_STAGGER * index,
+          target=(col, row)
+      ))
+      index += 1
 
   def get_item_at(ctx, cell):
     inventory = ctx.data
@@ -42,6 +68,9 @@ class InventoryContext(Context):
     return col >= 0 and col < cols and row >= 0 and row < rows
 
   def handle_keydown(ctx, key):
+    if ctx.anims:
+      return False
+
     key_deltas = {
       pygame.K_LEFT: (-1, 0),
       pygame.K_RIGHT: (1, 0),
@@ -89,10 +118,9 @@ class InventoryContext(Context):
     box = assets.sprites["box"]
 
     cols, rows = ctx.grid_size
-    width = max(0, cols * (box.get_width() + SPACING) - SPACING)
-    height = max(0, rows * (box.get_height() + SPACING) - SPACING)
-
-    menu = Surface((width, height))
+    menu_width = max(0, cols * (box.get_width() + SPACING) - SPACING)
+    menu_height = max(0, rows * (box.get_height() + SPACING) - SPACING)
+    menu = Surface((menu_width, menu_height))
     menu.set_colorkey(0xFF00FF)
     menu.fill(0xFF00FF)
 
@@ -106,7 +134,7 @@ class InventoryContext(Context):
         new_color = None
         if item is None:
           new_color = palette.GRAY
-        elif (col, row) == ctx.cursor:
+        elif (col, row) == ctx.cursor and not ctx.anims:
           new_color = palette.YELLOW
         if new_color:
           sprite = replace_color(
@@ -114,8 +142,19 @@ class InventoryContext(Context):
             old_color=palette.WHITE,
             new_color=new_color
           )
-        menu.blit(sprite, (x, y))
-        if item:
+        t = 1
+        box_width = box.get_width()
+        box_height = box.get_height()
+        for anim in ctx.anims:
+          if anim.target == cell:
+            t = ease_out(anim.update())
+            box_width = round(box_width * t)
+            box_height = round(box_height)
+            sprite = pygame.transform.scale(sprite, (box_width, box_height))
+            if anim.done:
+              ctx.anims.remove(anim)
+        menu.blit(sprite, (x + box.get_width() // 2 - box_width // 2, y))
+        if item and not ctx.anims:
           sprite = ctx.render_item(item)
           menu.blit(sprite, (x + 8, y + 8))
         x += box.get_width() + SPACING
