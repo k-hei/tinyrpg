01c1e58083c1d60bd6c0db59236ed11304364873
Brandon Semilla <semibran@gmail.com>
2021-04-18 00:58:45 -0700

    piece call animation

--- src/contexts/custom.py
+++ src/contexts/custom.py
@@ -30,6 +30,7 @@ class ExitAnim(TweenAnim): pass
 class SelectAnim(TweenAnim): pass
 class DeselectAnim(TweenAnim): pass
 class PlaceAnim(TweenAnim): pass
+class CallAnim(TweenAnim): pass
 class RecallAnim(TweenAnim): pass
 
 class CustomContext(Context):
@@ -156,28 +157,29 @@ class CustomContext(Context):
     skill = ctx.get_selected_skill()
     if not ctx.is_skill_used(skill):
       ctx.arrange = True
-    else:
-      game = ctx.parent
-      ctx.arrange = True
-      other = game.hero if ctx.char is game.ally else game.ally
-      char_skills = [skill for skill, cell in game.skill_builds[ctx.char]]
-      other_skills = [skill for skill, cell in game.skill_builds[other]]
-      if skill in char_skills:
-        index = char_skills.index(skill)
-        build = game.skill_builds[ctx.char]
-        _, cell = build.pop(index)
-        ctx.cursor = cell
-      elif skill in other_skills:
-        index = other_skills.index(skill)
-        build = game.skill_builds[other]
-        build.pop(index)
-        ctx.cursor = (0, 0)
+      ctx.anims.append([CallAnim(duration=7, target=(skill, (0, 0)))])
+      return
+    game = ctx.parent
+    ctx.arrange = True
+    other = game.hero if ctx.char is game.ally else game.ally
+    char_skills = [skill for skill, cell in game.skill_builds[ctx.char]]
+    other_skills = [skill for skill, cell in game.skill_builds[other]]
+    if skill in char_skills:
+      index = char_skills.index(skill)
+      build = game.skill_builds[ctx.char]
+      _, cell = build.pop(index)
+      ctx.cursor = cell
+    elif skill in other_skills:
+      index = other_skills.index(skill)
+      build = game.skill_builds[other]
+      build.pop(index)
+      ctx.cursor = (0, 0)
 
   def handle_keydown(ctx, key):
     blocking = False
     for group in ctx.anims:
       for anim in group:
-        if type(anim) is EnterAnim or type(anim) is ExitAnim:
+        if type(anim) in (EnterAnim, ExitAnim, CallAnim, RecallAnim):
           blocking = True
           break
     if blocking or keyboard.get_pressed(key) != 1:
@@ -366,38 +368,43 @@ class CustomContext(Context):
         sprite = Piece.render(skill.blocks, Skill.get_color(skill), Skill.get_icon(skill))
         surface.blit(sprite, (x, y))
 
-    # selected piece
-    cursor_x += (target_x - cursor_x) / 4
-    cursor_y += (target_y - cursor_y) / 4
-    ctx.cursor_drawn = (cursor_x, cursor_y)
-    if ctx.arrange:
-      blocks = skill_sel.blocks
-      sprite = Piece.render(blocks, Skill.get_color(skill_sel), Skill.get_icon(skill_sel))
-      surface.blit(sprite, (
-        grid_x + cursor_x * Piece.BLOCK_SIZE,
-        grid_y + cursor_y * Piece.BLOCK_SIZE - 4,
-      ))
-
     pieces = []
-    recalling = None
+    piece_anim = None
     for group in ctx.anims:
       for anim in group:
-        if type(anim) is RecallAnim:
-          recalling = anim
+        if type(anim) in (CallAnim, RecallAnim):
+          piece_anim = anim
           break
-    if recalling:
-      skill, (col, row) = recalling.target
+    if piece_anim:
+      skill, (col, row) = piece_anim.target
       sprite = Piece.render(skill.blocks, Skill.get_color(skill_sel), Skill.get_icon(skill_sel))
       i = skills.index(skill)
       start_x = grid_x + col * Piece.BLOCK_SIZE
       start_y = grid_y + row * Piece.BLOCK_SIZE - 4
       end_x = deck.get_width() + SPACING_X
       end_y = deck_y + (i - ctx.offset) * (assets.sprites["skill"].get_height() + SKILL_MARGIN_Y)
-      t = ease_out(recalling.pos)
+      t = ease_out(piece_anim.pos)
+      if type(piece_anim) is CallAnim:
+        t = 1 - t
       x = lerp(start_x, end_x, t)
       y = lerp(start_y, end_y, t)
-      if recalling.time < recalling.duration // 2 or recalling.time % 2:
+      if type(piece_anim) is CallAnim or (
+        piece_anim.time < piece_anim.duration // 2
+        or piece_anim.time % 2
+      ):
         pieces.append((sprite, x, y))
+
+    # selected piece
+    cursor_x += (target_x - cursor_x) / 4
+    cursor_y += (target_y - cursor_y) / 4
+    ctx.cursor_drawn = (cursor_x, cursor_y)
+    if ctx.arrange and not piece_anim:
+      blocks = skill_sel.blocks
+      sprite = Piece.render(blocks, Skill.get_color(skill_sel), Skill.get_icon(skill_sel))
+      surface.blit(sprite, (
+        grid_x + cursor_x * Piece.BLOCK_SIZE,
+        grid_y + cursor_y * Piece.BLOCK_SIZE - 4,
+      ))
 
     badges = []
     visible_start = ctx.offset
