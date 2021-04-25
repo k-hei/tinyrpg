d28847f0fd702db4dda24500c3a1abc56d5cd9d4
Brandon Semilla <semibran@gmail.com>
2021-04-25 15:13:53 -0700

    flinch animation when receiving damage

--- src/comps/hud.py
+++ src/comps/hud.py
@@ -1,4 +1,5 @@
 import math
+import random
 import pygame
 from pygame import Surface, Rect, PixelArray
 
@@ -10,6 +11,7 @@ from text import render_char, render as render_text
 from actors.knight import Knight
 from actors.mage import Mage
 
+from anims import Anim
 from anims.tween import TweenAnim
 from easing.expo import ease_out, ease_in
 from lerp import lerp
@@ -30,23 +32,55 @@ HP_BAR_Y2 = 28
 HP_HALFWIDTH = 13
 ENTER_DURATION = 8
 EXIT_DURATION = 6
+FLINCH_DURATION = 10
+
+class EnterAnim(TweenAnim):
+  def __init__(anim):
+    super().__init__(duration=ENTER_DURATION)
+
+class ExitAnim(TweenAnim):
+  def __init__(anim):
+    super().__init__(duration=EXIT_DURATION)
+
+class FlinchAnim(Anim):
+  def __init__(anim):
+    super().__init__(duration=FLINCH_DURATION)
 
 class Hud:
   def __init__(panel):
-    panel.surface = None
+    panel.sprite = None
     panel.active = True
     panel.anims = []
+    panel.hero = None
+    panel.hp_hero = math.inf
+    panel.hp_ally = math.inf
+    panel.offset = (0, 0)
     panel.enter()
 
   def enter(panel):
     panel.active = True
-    panel.anims.append(TweenAnim(duration=ENTER_DURATION))
+    panel.anims.append(EnterAnim())
 
   def exit(panel):
     panel.active = False
-    panel.anims.append(TweenAnim(duration=EXIT_DURATION))
+    panel.anims.append(ExitAnim())
 
-  def render(panel, ctx):
+  def update(panel, hero, ally):
+    if (panel.sprite is None
+    or hero != panel.hero
+    or hero.hp != panel.hp_hero
+    or ally.hp != panel.hp_ally):
+      if hero != panel.hero:
+        panel.hero = hero
+        panel.hp_hero = hero.hp
+        panel.hp_ally = ally.hp
+      elif hero.hp < panel.hp_hero or ally.hp < panel.hp_ally:
+        panel.anims.append(FlinchAnim())
+        panel.hp_hero = hero.hp
+        panel.hp_ally = ally.hp
+      panel.sprite = panel.render(hero, ally)
+
+  def render(panel, hero, ally):
     assets = use_assets()
     width = assets.sprites["hud"].get_width() + 1
     height = assets.sprites["hud"].get_height()
@@ -55,17 +89,17 @@ class Hud:
     sprite.set_colorkey(0xFF00FF)
     sprite.blit(assets.sprites["hud"], (0, 0))
 
-    if type(ctx.hero) is Knight:
+    if type(hero) is Knight:
       hero_portrait = assets.sprites["circle_knight"]
       ally_portrait = assets.sprites["circ16_mage"]
-    elif type(ctx.hero) is Mage:
+    elif type(hero) is Mage:
       hero_portrait = assets.sprites["circle_mage"]
       ally_portrait = assets.sprites["circ16_knight"]
 
-    if ctx.hero.dead:
+    if hero.dead:
       hero_portrait = replace_color(hero_portrait, palette.WHITE, palette.BLACK)
       hero_portrait = replace_color(hero_portrait, palette.BLUE, palette.RED)
-    if ctx.ally.dead:
+    if ally.dead:
       ally_portrait = replace_color(ally_portrait, palette.WHITE, palette.BLACK)
       ally_portrait = replace_color(ally_portrait, palette.BLUE, palette.RED)
 
@@ -73,7 +107,7 @@ class Hud:
     sprite.blit(ally_portrait, (CIRC16_X, CIRC16_Y))
     sprite.blit(assets.sprites["hp"], (HP_X, HP_Y))
 
-    hp_text = str(math.ceil(max(0, ctx.hero.hp)))
+    hp_text = str(math.ceil(max(0, hero.hp)))
     gray = False
     if len(hp_text) == 1:
       hp_text = "0" + hp_text
@@ -92,32 +126,42 @@ class Hud:
       sprite.blit(number, (x, HP_VALUE_Y))
       x += number.get_width() - 2
 
-    number = render_text(str(ctx.hero.hp_max), assets.fonts["smallcaps"])
+    number = render_text(str(hero.hp_max), assets.fonts["smallcaps"])
     number = outline(number, palette.BLACK)
     number = outline(number, palette.WHITE)
     sprite.blit(number, (HP_MAX_X, HP_MAX_Y))
 
-    draw_bar(sprite, ctx.hero.hp / ctx.hero.hp_max, HP_BAR_X, HP_BAR_Y1)
-    draw_bar(sprite, ctx.ally.hp / ctx.ally.hp_max, HP_BAR_X, HP_BAR_Y2)
-
+    draw_bar(sprite, hero.hp / hero.hp_max, HP_BAR_X, HP_BAR_Y1)
+    draw_bar(sprite, ally.hp / ally.hp_max, HP_BAR_X, HP_BAR_Y2)
     return sprite
 
   def draw(panel, surface, ctx):
-    sprite = panel.render(ctx)
+    panel.update(ctx.hero, ctx.ally)
+    sprite = panel.sprite
     hidden_x, hidden_y = MARGIN_LEFT, -sprite.get_height()
     corner_x, corner_y = MARGIN_LEFT, MARGIN_TOP
     anim = panel.anims[0] if panel.anims else None
     if anim:
       t = anim.update()
-      if panel.active:
+      if type(anim) is EnterAnim:
         t = ease_out(t)
         start_x, start_y = hidden_x, hidden_y
         target_x, target_y = corner_x, corner_y
-      else:
+      elif type(anim) is ExitAnim:
         start_x, start_y = corner_x, corner_y
         target_x, target_y = hidden_x, hidden_y
-      x = lerp(start_x, target_x, t)
-      y = lerp(start_y, target_y, t)
+      elif type(anim) is FlinchAnim:
+        offset_x, offset_y = panel.offset
+        while (offset_x, offset_y) == panel.offset:
+          offset_x = random.randint(-1, 1)
+          offset_y = random.randint(-1, 1)
+        x = corner_x + offset_x
+        y = corner_y + offset_y
+
+      if type(anim) is EnterAnim or type(anim) is ExitAnim:
+        x = lerp(start_x, target_x, t)
+        y = lerp(start_y, target_y, t)
+
       if anim.done:
         panel.anims.pop(0)
     elif panel.active:
