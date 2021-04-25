656db1c2880bcfc148db3e0a2317c58c7d5ab744
Brandon Semilla <semibran@gmail.com>
2021-04-25 13:17:22 -0700

    ctrl+minus/equals to shrink/expand

--- src/config.py
+++ src/config.py
@@ -2,8 +2,9 @@ fps = 60
 TILE_SIZE = 32
 window_width = 256
 window_height = 224
-window_scale = 2
 window_size = (window_width, window_height)
+SCALE_INIT = 2
+SCALE_MAX = 6
 
 MOVE_DURATION = 16
 ATTACK_DURATION = 12
--- src/main.py
+++ src/main.py
@@ -10,14 +10,25 @@ from contexts.dungeon import DungeonContext
 
 WINDOW_TITLE = "tinyrpg"
 WINDOW_SIZE = (config.window_width, config.window_height)
-WINDOW_SIZE_SCALED = (
-  config.window_width * config.window_scale,
-  config.window_height * config.window_scale
-)
+window_scale = config.SCALE_INIT
 
 pygame.display.init()
 pygame.display.set_caption(WINDOW_TITLE)
-display = pygame.display.set_mode(WINDOW_SIZE_SCALED)
+display = None
+window_size_scaled = None
+
+def resize(new_scale):
+  if new_scale < 1 or new_scale > config.SCALE_MAX:
+    return False
+  global window_scale, window_size_scaled, display
+  window_scale = new_scale
+  new_width = config.window_width * new_scale
+  new_height = config.window_height * new_scale
+  window_size_scaled = (new_width, new_height)
+  display = pygame.display.set_mode(window_size_scaled)
+  return True
+
+resize(window_scale)
 surface = Surface(WINDOW_SIZE)
 pygame.key.set_repeat(1000 // config.fps)
 
@@ -30,6 +41,14 @@ game_ctx.child = DungeonContext(parent=game_ctx)
 def handle_keydown(key):
   keyboard.handle_keydown(key)
   game_ctx.handle_keydown(key)
+  if keyboard.get_pressed(key) > 1:
+    return
+  ctrl = (keyboard.get_pressed(pygame.K_LCTRL)
+      or keyboard.get_pressed(pygame.K_RCTRL))
+  if key == pygame.K_MINUS and ctrl:
+    resize(window_scale - 1)
+  elif key == pygame.K_EQUALS and ctrl:
+    resize(window_scale + 1)
 
 def handle_keyup(key):
   keyboard.handle_keyup(key)
@@ -37,7 +56,7 @@ def handle_keyup(key):
 
 def render():
   game_ctx.draw(surface)
-  display.blit(pygame.transform.scale(surface, WINDOW_SIZE_SCALED), (0, 0))
+  display.blit(pygame.transform.scale(surface, window_size_scaled), (0, 0))
   pygame.display.flip()
 
 done = False
