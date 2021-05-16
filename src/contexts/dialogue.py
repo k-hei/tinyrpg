834add0439e6bb73f6924e74b299e118b460275f
Brandon Semilla <semibran@gmail.com>
2021-05-16 02:00:56 -0700

    dialogue black bars

--- src/contexts/dialogue.py
+++ src/contexts/dialogue.py
@@ -1,18 +1,28 @@
 from math import pi, sin
 import pygame
+from pygame import Rect
 import config
 import keyboard
 from contexts import Context
 from assets import load as use_assets
 from comps.log import Log
 from hud import Hud
+from palette import BLACK
+from anims.tween import TweenAnim
+from easing.expo import ease_out
 # from comps.previews import Previews
 # from comps.minimap import Minimap
 # from comps.spmeter import SpMeter
 # from comps.floorno import FloorNo
 
+class EnterAnim(TweenAnim): pass
+class ExitAnim(TweenAnim): pass
+
 class DialogueContext(Context):
   effects = [Hud] # , Previews, Minimap, SpMeter, FloorNo]
+  BAR_HEIGHT = 24
+  BAR_ENTER_DURATION = 15
+  BAR_EXIT_DURATION = 7
 
   def __init__(ctx, script, on_close=None):
     super().__init__(on_close=on_close)
@@ -20,7 +30,14 @@ class DialogueContext(Context):
     ctx.index = 0
     ctx.name = None
     ctx.log = Log(autohide=False)
-    ctx.print()
+    ctx.enter()
+
+  def enter(ctx):
+    ctx.anim = EnterAnim(duration=ctx.BAR_ENTER_DURATION, on_end=ctx.print)
+
+  def exit(ctx):
+    ctx.anim = ExitAnim(duration=ctx.BAR_EXIT_DURATION, on_end=ctx.close)
+    ctx.log.exit()
 
   def print(ctx, item=None):
     if item is None:
@@ -54,12 +71,28 @@ class DialogueContext(Context):
       return ctx.log.skip()
     ctx.index += 1
     if ctx.index == len(ctx.script):
-      return ctx.log.exit(on_end=ctx.close)
+      return ctx.exit()
     ctx.print()
 
   def draw(ctx, surface):
     assets = use_assets()
     sprite_arrow = assets.sprites["arrow_dialogue"]
+
+    bar_height = ctx.BAR_HEIGHT
+    if ctx.anim:
+      anim = ctx.anim
+      if anim.done:
+        ctx.anim = None
+      anim.update()
+      t = anim.pos
+      if type(anim) is EnterAnim:
+        t = ease_out(t)
+      elif type(anim) is ExitAnim:
+        t = 1 - t
+      bar_height *= t
+    pygame.draw.rect(surface, BLACK, Rect(0, 0, surface.get_width(), bar_height))
+    pygame.draw.rect(surface, BLACK, Rect(0, surface.get_height() - bar_height + 1, surface.get_width(), bar_height))
+
     ctx.log.update()
     sprite = ctx.log.box
     if sprite:
@@ -72,4 +105,5 @@ class DialogueContext(Context):
         x += -sprite_arrow.get_width() + sprite.get_width() - Log.PADDING_X - 8
         y += -sprite_arrow.get_height() + sprite.get_height() - Log.PADDING_Y + 4 + offset
         surface.blit(sprite_arrow, (x, y))
+
     super().draw(surface)
