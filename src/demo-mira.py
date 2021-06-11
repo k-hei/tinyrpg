afc423dbedb0c9951458ffa322ac9a7d542af2d9
Brandon Semilla <semibran@gmail.com>
2021-06-11 15:43:57 -0700

    talking

--- src/demo-mira.py
+++ src/demo-mira.py
@@ -1,32 +1,53 @@
+import pygame
 from contexts import Context
 from contexts.app import App
 from assets import load as use_assets
 from palette import WHITE
 from anims.frame import FrameAnim
+import keyboard
 
 class MiraPortrait:
   EYES_POS = (52, 38)
   MOUTH_POS = (61, 59)
   BLINK_INTERVAL = 150
+  BLINK_DURATION = 16
   BLINK_FRAMES = ["mira_eyes", "mira_eyes_closing", "mira_eyes_closed", "mira_eyes_closing"]
+  TALK_DURATION = 16
   TALK_FRAMES = ["mira_mouth", "mira_mouth_opening", "mira_mouth_open", "mira_mouth_opening"]
 
   class BlinkAnim(FrameAnim):
     def __init__(anim, *args, **kwargs):
-      super().__init__(frames=MiraPortrait.BLINK_FRAMES, *args, **kwargs)
+      super().__init__(
+        frames=MiraPortrait.BLINK_FRAMES,
+        duration=MiraPortrait.BLINK_DURATION,
+        *args, **kwargs
+      )
 
-  class TalkAnim(FrameAnim): pass
+  class TalkAnim(FrameAnim):
+    def __init__(anim, *args, **kwargs):
+      super().__init__(
+        frames=MiraPortrait.TALK_FRAMES,
+        duration=MiraPortrait.TALK_DURATION,
+        *args, **kwargs
+      )
 
   def __init__(portrait):
+    portrait.talking = False
     portrait.anims = []
     portrait.ticks = 0
 
   def blink(portrait):
-    portrait.anims.append(MiraPortrait.BlinkAnim(duration=16))
+    portrait.anims.append(MiraPortrait.BlinkAnim())
 
-  # def start_talk(portrait):
-  #   portrait.anims.append(MiraPortrait.TalkAnim(
-  #   ))
+  def start_talk(portrait):
+    portrait.talking = True
+    portrait.anims.append(MiraPortrait.TalkAnim(on_end=portrait.start_talk))
+
+  def stop_talk(portrait):
+    portrait.talking = False
+    talk_anim = next((a for a in portrait.anims if type(a) is MiraPortrait.TalkAnim), None)
+    if talk_anim:
+      portrait.anims.remove(talk_anim)
 
   def update(portrait):
     for anim in portrait.anims:
@@ -42,13 +63,14 @@ class MiraPortrait:
     portrait.update()
     assets = use_assets().sprites
     image = assets["mira"].copy()
-    image.blit(assets["mira_mouth"], MiraPortrait.MOUTH_POS)
 
-    eyes_frame = "mira_eyes"
-    eyes_anim = next((a for a in portrait.anims if type(a) is MiraPortrait.BlinkAnim), None)
-    if eyes_anim:
-      eyes_frame = eyes_anim.frame
+    blink_anim = next((a for a in portrait.anims if type(a) is MiraPortrait.BlinkAnim), None)
+    eyes_frame = blink_anim.frame if blink_anim else "mira_eyes"
     image.blit(assets[eyes_frame], MiraPortrait.EYES_POS)
+
+    talk_anim = next((a for a in portrait.anims if type(a) is MiraPortrait.TalkAnim), None)
+    mouth_frame = talk_anim.frame if talk_anim else "mira_mouth"
+    image.blit(assets[mouth_frame], MiraPortrait.MOUTH_POS)
 
     return image
 
@@ -56,6 +78,13 @@ class PortraitContext(Context):
   def __init__(ctx):
     super().__init__()
     ctx.portrait = MiraPortrait()
+
+  def handle_keydown(ctx, key):
+    if keyboard.get_pressed(key) > 1:
+      return
+
+    if key == pygame.K_SPACE:
+      return ctx.portrait.start_talk() if not ctx.portrait.talking else ctx.portrait.stop_talk()
 
   def draw(ctx, surface):
     surface.fill(WHITE)
