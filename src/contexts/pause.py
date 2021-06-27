324da1a93a000799fb00b1ec9c01ad4cb6071d72
Brandon Semilla <semibran@gmail.com>
2021-06-26 19:59:12 -0700

    rewire pause menu

--- src/contexts/pause.py
+++ src/contexts/pause.py
@@ -7,11 +7,13 @@ from contexts import Context
 from assets import load as use_assets
 from filters import outline, recolor, replace_color
 from palette import WHITE, BLUE, BLACK, GOLD
+from config import WINDOW_SIZE, WINDOW_HEIGHT
 from comps.hud import Hud
 from comps.previews import Previews
 from comps.minimap import Minimap
 from comps.spmeter import SpMeter
 from comps.floorno import FloorNo
+from sprite import Sprite
 
 MARGIN_X = 48
 MARGIN_Y = 48
@@ -33,26 +35,36 @@ class PauseContext(Context):
     if key == pygame.K_ESCAPE:
       return ctx.close()
 
-  def draw(ctx, surface):
+  def view(ctx):
+    sprites = []
     assets = use_assets()
 
     # tint
-    tint = Surface(surface.get_size(), pygame.SRCALPHA)
+    tint = Surface(WINDOW_SIZE, pygame.SRCALPHA)
     tint.fill(0x7F000000)
-    surface.blit(tint, (0, 0))
+    sprites.append(Sprite(
+      image=tint,
+      pos=(0, 0)
+    ))
 
     # gold
     gold_image = assets.sprites["item_gold"]
     gold_image = replace_color(gold_image, BLACK, GOLD)
     gold_x = MARGIN_X
-    gold_y = surface.get_height() - MARGIN_Y
-    surface.blit(gold_image, (gold_x, gold_y))
+    gold_y = WINDOW_HEIGHT - MARGIN_Y
+    sprites.append(Sprite(
+      image=gold_image,
+      pos=(gold_x, gold_y)
+    ))
 
     gold_amount = ctx.parent.get_gold()
     gold_text = assets.ttf["english"].render("{}G".format(gold_amount))
     gold_x += gold_image.get_width() + GOLD_SPACING
     gold_y += gold_image.get_height() // 2 - gold_text.get_height() // 2
-    surface.blit(gold_text, (gold_x, gold_y))
+    sprites.append(Sprite(
+      image=gold_text,
+      pos=(gold_x, gold_y)
+    ))
 
     # choices
     choices_width = 0
@@ -64,21 +76,33 @@ class PauseContext(Context):
         choices_width = text_image.get_width()
       x = MARGIN_X
       y = i * (text_image.get_height() + SPACING) + MARGIN_Y
-      surface.blit(text_image, (x, y))
+      sprites.append(Sprite(
+        image=text_image,
+        pos=(x, y)
+      ))
 
     # hand
     hand_image = flip(assets.sprites["hand"], True, False)
     hand_x = MARGIN_X - hand_image.get_width() - HAND_SPACING
     hand_y = MARGIN_Y
-    surface.blit(hand_image, (hand_x, hand_y))
+    sprites.append(Sprite(
+      image=hand_image,
+      pos=(hand_x, hand_y)
+    ))
 
     # hud
     hud_image = assets.sprites["hud_single"]
     hud_x = MARGIN_X + choices_width + HUD_MARGIN
     hud_y = MARGIN_Y
-    surface.blit(hud_image, (hud_x, hud_y))
-    surface.blit(assets.sprites["circle_knight"], (hud_x, hud_y + 1))
+    sprites += [
+      Sprite(image=hud_image, pos=(hud_x, hud_y)),
+      Sprite(image=assets.sprites["circle_knight"], pos=(hud_x, hud_y + 1))
+    ]
 
     hud_y += hud_image.get_height() + HUD_MARGIN
-    surface.blit(hud_image, (hud_x, hud_y))
-    surface.blit(assets.sprites["circle_mage"], (hud_x, hud_y + 1))
+    sprites += [
+      Sprite(image=hud_image, pos=(hud_x, hud_y)),
+      Sprite(image=assets.sprites["circle_mage"], pos=(hud_x, hud_y + 1))
+    ]
+
+    return sprites
