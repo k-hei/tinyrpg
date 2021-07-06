2ff3ae1eff69bb6d194fefcbee37407052255a80
Brandon Semilla <semibran@gmail.com>
2021-07-05 19:14:50 -0700

    shiny thing in altar core

--- src/dungeon/props/altar.py
+++ src/dungeon/props/altar.py
@@ -1,12 +1,37 @@
 from dungeon.element import DungeonElement
 from assets import load as use_assets
 from filters import replace_color
-from palette import WHITE, GOLD
+from palette import WHITE, BLACK, GOLD, VIOLET
 from sprite import Sprite
+from anims.frame import FrameAnim
 
 class Altar(DungeonElement):
+  def __init__(altar, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    altar.reset_anim()
+
+  def reset_anim(altar):
+    altar.anim = FrameAnim(
+      frames=["fx_soul{}".format(i) for i in range(6)],
+      duration=60
+    )
+
+  def update(altar):
+    altar.anim.update()
+    if altar.anim.done:
+      altar.reset_anim()
+
   def view(altar, anims):
-    return super().view(Sprite(
-      image=replace_color(use_assets().sprites["altar"], WHITE, GOLD),
+    assets = use_assets()
+    sprites = [Sprite(
+      image=replace_color(assets.sprites["altar"], WHITE, GOLD),
       layer="elems"
-    ), anims)
+    )]
+    if altar.anim.time % 2:
+      sprites.append(Sprite(
+        image=replace_color(assets.sprites[altar.anim.frame], BLACK, VIOLET),
+        pos=(0, -24),
+        origin=("center", "center"),
+        layer="numbers"
+      ))
+    return super().view(sprites, anims)
