1664739826058f453ded1d0fe51577db2e5c0d17
Brandon Semilla <semibran@gmail.com>
2021-08-18 21:35:48 -0700

    eyeball directionals

--- src/dungeon/actors/eye.py
+++ src/dungeon/actors/eye.py
@@ -2,7 +2,7 @@ from random import randint, choice
 from lib.cell import is_adjacent
 from dungeon.actors import DungeonActor
 from cores import Core, Stats
-from assets import load as use_assets
+from assets import sprites
 from skills.weapon.tackle import Tackle
 from skills.ailment.steal import Steal
 from skills.armor.hpup import HpUp
@@ -16,6 +16,38 @@ from sprite import Sprite
 from filters import replace_color
 from colors.palette import BLACK, CYAN
 from config import PUSH_DURATION
+
+def IdleSprite(facing):
+  if facing == (0, 1):
+    return sprites["eyeball"]
+  elif facing == (0, -1):
+    return sprites["eyeball_up"]
+  elif facing[0]:
+    return sprites["eyeball_right"]
+
+def MoveSprite(facing):
+  if facing == (0, 1):
+    return sprites["eyeball_move"]
+  elif facing == (0, -1):
+    return sprites["eyeball_move_up"]
+  elif facing[0]:
+    return sprites["eyeball_move_right"]
+
+def FlinchSprite(facing):
+  if facing == (0, 1):
+    return sprites["eyeball_flinch"]
+  elif facing == (0, -1):
+    return sprites["eyeball_flinch_up"]
+  elif facing[0]:
+    return sprites["eyeball_flinch_right"]
+
+def SleepSprite(facing):
+  if facing == (0, 1):
+    return sprites["eyeball_sleep"]
+  elif facing == (0, -1):
+    return sprites["eyeball_sleep_up"]
+  elif facing[0]:
+    return sprites["eyeball_sleep_right"]
 
 class Eyeball(DungeonActor):
   drops = [AngelTears]
@@ -40,33 +72,32 @@ class Eyeball(DungeonActor):
       eye.promote(hp=False)
       eye.core.skills.append(HpUp)
 
-  def view(eye, anims):
-    sprites = use_assets().sprites
+  def view(eyeball, anims):
     sprite = None
     for anim_group in anims:
-      for anim in [a for a in anim_group if a.target is eye]:
+      for anim in [a for a in anim_group if a.target is eyeball]:
         if type(anim) is AwakenAnim:
-          return super().view(sprites["eyeball_move"], anims)
-    if eye.is_dead():
-      return super().view(sprites["eyeball_flinch"], anims)
-    anim_group = [a for a in anims[0] if a.target is eye] if anims else []
+          return super().view(MoveSprite(eyeball.facing), anims)
+    if eyeball.is_dead():
+      return super().view(FlinchSprite(eyeball.facing), anims)
+    anim_group = [a for a in anims[0] if a.target is eyeball] if anims else []
     for anim in anim_group:
       if type(anim) is MoveAnim and anim.duration != PUSH_DURATION:
-        sprite = sprites["eyeball_move"]
+        sprite = MoveSprite(eyeball.facing)
         break
       elif (type(anim) is AttackAnim
       and anim.time >= 0
       and anim.time < anim.duration // 2):
-        sprite = sprites["eyeball_move"]
+        sprite = MoveSprite(eyeball.facing)
         break
       elif type(anim) in (FlinchAnim, FlickerAnim):
-        sprite = sprites["eyeball_flinch"]
+        sprite = FlinchSprite(eyeball.facing)
         break
     else:
-      if eye.ailment == "sleep":
-        sprite = sprites["eyeball_sleep"]
+      if eyeball.ailment == "sleep":
+        sprite = SleepSprite(eyeball.facing)
       else:
-        sprite = sprites["eyeball"]
-    if eye.ailment == "freeze":
-      sprite = sprites["eyeball_flinch"]
+        sprite = IdleSprite(eyeball.facing)
+    if eyeball.ailment == "freeze":
+      sprite = FlinchSprite(eyeball.facing)
     return super().view(sprite, anims)
