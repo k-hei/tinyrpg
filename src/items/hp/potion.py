6fbdfa8dc6e043299b3a315051856b6746addbd8
Brandon Semilla <semibran@gmail.com>
2021-09-03 17:25:11 -0700

    add potion throw effect

--- src/items/hp/potion.py
+++ src/items/hp/potion.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
 from items.hp import HpItem
+from vfx.burst import BurstVfx
+from comps.damage import DamageValue
+from colors.palette import GREEN
 
 @dataclass
 class Potion(HpItem):
@@ -7,3 +10,12 @@ class Potion(HpItem):
   desc: str = "Restores\n20 HP."
   hp: int = 20
   value: int = 25
+
+  def effect(potion, game, actor=None):
+    game.vfx.append(BurstVfx(
+      cell=actor.cell,
+      color=potion.color
+    ))
+    actor = actor or game.hero
+    actor.regen(potion.hp)
+    game.numbers.append(DamageValue(str(potion.hp), actor.cell, color=GREEN))
