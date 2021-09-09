e72205c490dc5f65791cea6cff5786559ea165d9
Brandon Semilla <semibran@gmail.com>
2021-09-08 18:49:09 -0700

    elevated prop pushing

--- src/dungeon/props/__init__.py
+++ src/dungeon/props/__init__.py
@@ -1,5 +1,6 @@
 from dungeon.element import DungeonElement
 from config import TILE_SIZE
+from anims.move import MoveAnim
 
 class Prop(DungeonElement):
   def effect(prop, game, actor=None):
@@ -10,6 +11,8 @@ class Prop(DungeonElement):
       return []
     sprite = sprites[0]
     prop_xoffset, prop_yoffset = (0, 0)
-    prop_yoffset -= prop.elev * TILE_SIZE
+    move_anim = anims and next((a for a in anims[0] if a.target is prop and isinstance(a, MoveAnim)), None)
+    if not move_anim:
+      prop_yoffset -= prop.elev * TILE_SIZE
     sprite.move((prop_xoffset, prop_yoffset))
     return super().view([sprite, *sprites[1:]], anims)
