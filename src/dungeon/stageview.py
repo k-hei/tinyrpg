543bb8c8fb76f17c18287ce9a9bc54d5728d5aee
Brandon Semilla <semibran@gmail.com>
2021-09-26 01:13:28 -0700

    draw alternate walls for certain rooms

--- src/dungeon/stageview.py
+++ src/dungeon/stageview.py
@@ -514,7 +514,8 @@ def render_wall(stage, cell, visited_cells):
   tile_below = stage.get_tile_at((x, y + 1))
   tile_base = stage.get_tile_at((x, y + 2))
   room = next((r for r in stage.rooms if cell in r.get_cells() + r.get_outline()), None)
-  elev = 1 if room and next((c for c in room.get_cells() if stage.get_tile_at(c) is stage.FLOOR_ELEV), None) else 0
+  is_elevated = room and next((c for c in room.get_cells() if stage.get_tile_at(c) is stage.FLOOR_ELEV), False)
+  is_special_room = not is_elevated and room and room.data and room.data.tiles
 
   if tile_below is stage.FLOOR_ELEV:
     return Sprite(image=assets.sprites["wall_bottom"], pos=(0, -TILE_SIZE))
@@ -526,11 +527,17 @@ def render_wall(stage, cell, visited_cells):
   )
   and (x, y + 1) in visited_cells
   and not stage.get_elem_at((x, y + 1), superclass=Door)):
-    if x % (3 + y % 2) == 0 or SecretDoor.exists_at(stage, cell):
-      return assets.sprites["wall_torch"]
+    if is_special_room:
+      if x % (2 + y % 2) == 0 or SecretDoor.exists_at(stage, cell):
+        return assets.sprites["wall_battle_alt"]
+      else:
+        return assets.sprites["wall_battle"]
     else:
-      return assets.sprites["wall_bottom"]
-  elif elev == 1 and tile_below is stage.WALL and not (tile_base is None or tile_base is stage.WALL):
+      if x % (3 + y % 2) == 0 or SecretDoor.exists_at(stage, cell):
+        return assets.sprites["wall_alt"]
+      else:
+        return assets.sprites["wall_bottom"]
+  elif is_elevated and tile_below is stage.WALL and not (tile_base is None or tile_base is stage.WALL):
     return assets.sprites["wall_top"]
   else:
     return render_walltop(stage, cell, visited_cells)
