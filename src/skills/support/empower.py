d4f7e3ff6278c00e7b953dc43593dd4cbaf6c03b
Brandon Semilla <semibran@gmail.com>
2022-03-13 00:37:58 -0800

    add empower vfx

--- src/skills/support/empower.py
+++ src/skills/support/empower.py
@@ -1,6 +1,8 @@
 from cores.knight import Knight
 from skills.support import SupportSkill
 from dungeon.status.atk import AtkUpEffect
+from vfx.atkup import AtkUpVfx
+from anims.pause import PauseAnim
 
 import debug
 
@@ -8,7 +10,7 @@ class Empower(SupportSkill):
   name = "Empower"
   desc = "Increases attack strength"
   element = "sword"
-  cost = 4
+  cost = 3
   range_min = 0
   range_max = 0
   users = (Knight),
@@ -19,5 +21,17 @@ class Empower(SupportSkill):
   ]
 
   def effect(game, user, dest=None, on_start=None, on_end=None):
-    user.apply_status_effect(AtkUpEffect())
+    game.anims.append([PauseAnim(
+      duration=45,
+      on_start=lambda: (
+        game.vfx.append(AtkUpVfx(cell=user.cell)),
+        user.core.anims.append(user.core.ChargeAnim()),
+        user.apply_status_effect(AtkUpEffect()),
+        on_start and on_start(),
+      ),
+      on_end=lambda: (
+        user.core.anims.pop(),
+        on_end and on_end(),
+      ),
+    )])
     debug.log("atk up")
