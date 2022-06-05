3f79975a1f6d7e62794904feb556f84ebad1868f
Brandon Semilla <semibran@gmail.com>
2022-06-05 15:28:12 -0700

    enable grid display in standalone dungeon rooms

--- src/contexts/combat/__init__.py
+++ src/contexts/combat/__init__.py
@@ -238,7 +238,7 @@ class CombatContext(ExploreBase):
 
     room = ctx.room
     room_cells = [c for c in room.cells
-      if not ctx.stage.is_tile_at_pit(c)]
+      if ctx.stage.is_cell_walkable(c)]
 
     room_cells = sorted(room_cells, key=lambda c: c[1] * ctx.stage.width + c[0])
     room_cells_by_dist = {}
@@ -1035,7 +1035,7 @@ class CombatContext(ExploreBase):
 
   def view_grid(ctx):
     room = ctx.room
-    if not room or room == ctx.stage.rooms[-1]:
+    if not room or ctx.stage.is_overworld and room == ctx.stage.rooms[-1]:
       return []
 
     topleft_pos = ctx.stage_view.camera.rect.topleft
@@ -1047,7 +1047,7 @@ class CombatContext(ExploreBase):
     rows = WINDOW_HEIGHT // TILE_SIZE + 2
 
     room_cells = [c for c in room.cells
-      if not ctx.stage.is_tile_at_pit(c)]
+      if ctx.stage.is_cell_walkable(c)]
 
     grid_cells = [(x, y)
       for y in range(top_row, top_row + rows)
