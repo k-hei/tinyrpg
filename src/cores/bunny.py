89d645d1cb2fb91e8fe16e836bfa078b56ad80dd
Brandon Semilla <semibran@gmail.com>
2022-06-05 00:46:51 -0700

    tweak slaver core file formatting

--- src/cores/bunny.py
+++ src/cores/bunny.py
@@ -7,53 +7,53 @@ from config import BUNNY_NAME
 
 
 class Bunny(Core):
-  name = BUNNY_NAME
-  sprites = SpriteMap(
-    face_right=assets.sprites["bunny"],
-    face_down=assets.sprites["bunny_down"],
-    face_up=assets.sprites["bunny_up"],
-    walk_right=assets.sprites["bunny_walk"],
-    walk_down=assets.sprites["bunny_walk_down"],
-    walk_up=assets.sprites["bunny_walk_up"],
-  )
+    name = BUNNY_NAME
+    sprites = SpriteMap(
+        face_right=assets.sprites["bunny"],
+        face_down=assets.sprites["bunny_down"],
+        face_up=assets.sprites["bunny_up"],
+        walk_right=assets.sprites["bunny_walk"],
+        walk_down=assets.sprites["bunny_walk_down"],
+        walk_up=assets.sprites["bunny_walk_up"],
+    )
 
-  class WalkAnim(WalkAnim):
-    period = 12
-    frame_count = 3
+    class WalkAnim(WalkAnim):
+        period = 12
+        frame_count = 3
 
-  def __init__(bunny, faction="enemy", *args, **kwargs):
-    super().__init__(
-      name=Bunny.name,
-      faction=faction,
-      *args, **kwargs)
+    def __init__(bunny, faction="enemy", *args, **kwargs):
+        super().__init__(
+            name=Bunny.name,
+            faction=faction,
+            *args, **kwargs)
 
-  def view(bunny):
-    surface = None
-    facing_x, facing_y = bunny.facing
-    flip_x, flip_y = facing_x == -1, False
+    def view(bunny):
+        surface = None
+        facing_x, facing_y = bunny.facing
+        flip_x, flip_y = facing_x == -1, False
 
-    for anim in bunny.anims:
-      if isinstance(anim, bunny.WalkAnim):
-        if facing_x == -1 or facing_x == 1:
-          walk_cycle = bunny.sprites.walk_right
-        elif facing_y == -1:
-          walk_cycle = bunny.sprites.walk_up
-        elif facing_y == 1:
-          walk_cycle = bunny.sprites.walk_down
-        surface = walk_cycle[anim.frame_index]
-        break
-    else:
-      if facing_x == 1 or facing_x == -1:
-        surface = bunny.sprites.face_right
-      elif facing_y == 1:
-        surface = bunny.sprites.face_down or bunny.sprites.face_right
-      elif facing_y == -1:
-        surface = bunny.sprites.face_up or bunny.sprites.face_right
+        for anim in bunny.anims:
+            if isinstance(anim, bunny.WalkAnim):
+                if facing_x == -1 or facing_x == 1:
+                    walk_cycle = bunny.sprites.walk_right
+                elif facing_y == -1:
+                    walk_cycle = bunny.sprites.walk_up
+                elif facing_y == 1:
+                    walk_cycle = bunny.sprites.walk_down
+                surface = walk_cycle[anim.frame_index]
+                break
+        else:
+            if facing_x == 1 or facing_x == -1:
+                surface = bunny.sprites.face_right
+            elif facing_y == 1:
+                surface = bunny.sprites.face_down or bunny.sprites.face_right
+            elif facing_y == -1:
+                surface = bunny.sprites.face_up or bunny.sprites.face_right
 
-    sprite = Sprite(
-      image=surface,
-      flip=(flip_x, flip_y),
-      layer="elems"
-    ) if surface else None
-    sprites = [sprite] if sprite else []
-    return super().view(sprites)
+        sprite = Sprite(
+            image=surface,
+            flip=(flip_x, flip_y),
+            layer="elems"
+        ) if surface else None
+        sprites = [sprite] if sprite else []
+        return super().view(sprites)
--- src/cores/mouse.py
+++ src/cores/mouse.py
@@ -1,24 +1,20 @@
-from cores import Stats
 from cores.biped import BipedCore, SpriteMap
-
-from assets import assets
-from anims.frame import FrameAnim
 from config import MOUSE_NAME
 
 
 class Mouse(BipedCore):
-  name = MOUSE_NAME
-  sprites = SpriteMap(
-    face_right="mouse",
-    face_down="mouse_down",
-    face_up="mouse_up",
-    walk_right=("mouse_walk", "mouse", "mouse_walk", "mouse"),
-    walk_down=("mouse_walkdown0", "mouse_down", "mouse_walkdown1", "mouse_down"),
-    walk_up=("mouse_walkup0", "mouse_up", "mouse_walkup1", "mouse_up")
-  )
+    name = MOUSE_NAME
+    sprites = SpriteMap(
+        face_right="mouse",
+        face_down="mouse_down",
+        face_up="mouse_up",
+        walk_right=("mouse_walk", "mouse", "mouse_walk", "mouse"),
+        walk_down=("mouse_walkdown0", "mouse_down", "mouse_walkdown1", "mouse_down"),
+        walk_up=("mouse_walkup0", "mouse_up", "mouse_walkup1", "mouse_up")
+    )
 
-  def __init__(mouse, faction="enemy", *args, **kwargs):
-    super().__init__(
-      name=Mouse.name,
-      faction=faction,
-      *args, **kwargs)
+    def __init__(mouse, *args, faction="enemy", **kwargs):
+        super().__init__(
+            name=Mouse.name,
+            faction=faction,
+            *args, **kwargs)
