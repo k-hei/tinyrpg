9dd492156587ffbd813072ac558c8611a2fda413
Brandon Semilla <semibran@gmail.com>
2022-06-08 14:28:10 -0700

    add port arg to room demos

--- src/demo-room.py
+++ src/demo-room.py
@@ -7,12 +7,21 @@ from contexts.explore.manifest import manifest_room
 from contexts.explore.roomdata import load_room
 from savedata import load
 
+
+argc = len(sys.argv)
+if argc not in (2, 3):
+  print("usage: demo-room.py room [port]"
+      "\n       demo-room.py tutorial1 right")
+  exit()
+
+
 room_name = sys.argv[1]
+room_port = sys.argv[2]
 room_data = load_room("rooms/", room_name)
 if isinstance(room_data, list):
   room_data = choice(room_data)
 
-stage = manifest_room(room_data)
+stage = manifest_room(room_data, room_port)
 
 savedata = load("src/data-debug.json")
 savedata.place = "dungeon"
