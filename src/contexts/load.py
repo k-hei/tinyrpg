508709611ff9947e32a946057cd3d53ddf636cbb
Brandon Semilla <semibran@gmail.com>
2022-06-07 21:14:08 -0700

    add project version to load screen

--- src/contexts/load.py
+++ src/contexts/load.py
@@ -7,6 +7,13 @@ from contexts.controls import ControlsContext
 from transits.dissolve import DissolveIn, DissolveOut
 from game.data import GameData
 
+import assets
+from lib.sprite import Sprite
+from lib.filters import outline
+from colors.palette import BLACK
+from config import WINDOW_WIDTH
+
+
 class LoadContext(DataContext):
   TITLE = "LOAD DATA"
   ACTION = "Load"
@@ -14,9 +21,18 @@ class LoadContext(DataContext):
 
   def enter(ctx):
     super().enter()
+    ctx.version = None
+
     ctx.can_close = type(ctx.parent).__name__ not in ("AppContext", "GameContext")
     if not ctx.can_close:
       ctx.EXTRA_CONTROLS = { "esc": "Controls" }
+      ctx.version = "STAGING"
+      try:
+        with open("VERSION", mode="r", encoding="utf-8") as version_file:
+          ctx.version = version_file.read().rstrip()
+      except FileNotFoundError:
+        pass
+
     ctx.anims[-1].on_end = lambda: ctx.open(
       DialogueContext(script=["Please select a file to load."], lite=True)
     )
@@ -48,3 +64,24 @@ class LoadContext(DataContext):
         ])
       ))
     ))
+
+  def view(ctx, *args, **kwargs):
+    sprites = super().view(*args, **kwargs)
+
+    if ctx.version:
+      try:
+        version_image = ctx.__version_image
+      except AttributeError:
+        version_text = ctx.version
+        version_image = assets.ttf["normal"].render(text=version_text)
+        version_image = outline(version_image, BLACK)
+        ctx.__version_image = version_image
+
+      sprites += [Sprite(
+        image=version_image,
+        pos=(WINDOW_WIDTH, 0),
+        origin=Sprite.ORIGIN_TOPRIGHT,
+        layer="ui",
+      )]
+
+    return sprites
