d2b2a7f0535dc3a377dffff13e7fb1b9c479a9af
Brandon Semilla <semibran@gmail.com>
2022-06-08 23:19:05 -0700

    switch to requirements.txt

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-py -m pip install --upgrade pygame pyinstaller pillow zstd
+py -m pip install -r requirements.txt
 py -m PyInstaller -Fwn tinyrpg src/demo.py $(py bin/build_datas.py)
 mv dist/tinyrpg.exe dist/tinyrpg-$(cat VERSION).exe
