5c40c6afe08830d3bca5f81052f3e3660030a9bc
Brandon Semilla <semibran@gmail.com>
2022-06-10 21:46:22 -0700

    add dungeon entrance elements

--- src/town/areas/tomb_entrance.py
+++ src/town/areas/tomb_entrance.py
@@ -2,12 +2,14 @@ import lib.vector as vector
 from lib.line import connect_lines
 from lib.sprite import Sprite
 
+from town.element import Element
 from town.sideview.stage import Area, AreaPort, AreaBgLayer
 import assets
 
 
 CAMERA_OFFSET = (0, -36)
 BG_LAYERGROUP_OFFSET = (160, -164)
+BG_FLOOR_OFFSET_Y = assets.sprites["tomb_entrance_building"].get_height() - 108
 
 class TombEntranceArea(Area):
     name = "Tomb Entrance"
@@ -20,12 +22,51 @@ class TombEntranceArea(Area):
         )),
         AreaBgLayer(sprite=Sprite(
             image=assets.sprites["tomb_entrance_stairs"],
-            layer="elems",
+            layer="fg",
             offset=assets.sprites["tomb_entrance_stairs"].get_height(),
             pos=vector.add(
                 BG_LAYERGROUP_OFFSET,
                 (-160, assets.sprites["tomb_entrance_building"].get_height() - 72),
             ),
+        )),
+    ]
+
+    elems = [
+        Element(sprite=Sprite(
+            image=assets.sprites["tomb_entrance_boxes"],
+            pos=vector.add(
+                BG_LAYERGROUP_OFFSET,
+                (176, BG_FLOOR_OFFSET_Y),
+            ),
+            origin=Sprite.ORIGIN_BOTTOMLEFT,
+            layer="bg",
+        )),
+        Element(sprite=Sprite(
+            image=assets.sprites["tomb_entrance_banner_left"],
+            pos=vector.add(
+                BG_LAYERGROUP_OFFSET,
+                (224, BG_FLOOR_OFFSET_Y - 96),
+            ),
+            origin=Sprite.ORIGIN_TOPLEFT,
+            layer="bg",
+        )),
+        Element(sprite=Sprite(
+            image=assets.sprites["tomb_entrance_banner_right"],
+            pos=vector.add(
+                BG_LAYERGROUP_OFFSET,
+                (320, BG_FLOOR_OFFSET_Y - 96),
+            ),
+            origin=Sprite.ORIGIN_TOPLEFT,
+            layer="bg",
+        )),
+        Element(sprite=Sprite(
+            image=assets.sprites["tomb_entrance_sign"],
+            pos=vector.add(
+                BG_LAYERGROUP_OFFSET,
+                (320, BG_FLOOR_OFFSET_Y - 64),
+            ),
+            origin=Sprite.ORIGIN_TOPLEFT,
+            layer="bg",
         )),
     ]
 
